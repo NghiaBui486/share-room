bf4d3b8832e2a6e9f7f32566e9ba0c31294fcb93
KhanhBui <buidunamkhanh4126@gmail.com>
2021-07-29 16:13:55 +0700

    update header, footer component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Row, Col, Space } from "antd";
 import {
-    FacebookOutlined,
-    YoutubeOutlined,
-    GooglePlusOutlined,
-  } from "@ant-design/icons";
+  FacebookOutlined,
+  YoutubeOutlined,
+  GooglePlusOutlined,
+} from "@ant-design/icons";
 import "./index.scss";
 
 function Footer() {
@@ -13,29 +14,47 @@ function Footer() {
       <Row>
         <Col className="column" span={6}>
           <p style={{ fontSize: 20 }}>CHÍNH SÁCH BẢO MẬT</p>
-          <p>An toàn mua bán</p>
-          <p>Quy định cần thiết</p>
-          <p>Chính sách riêng tư</p>
+          <p>
+            <Link className="column__link" to="/">An toàn mua bán</Link>
+          </p>
+          <p>
+            <Link className="column__link" to="/">Quy định cần thiết</Link>
+          </p>
+          <p>
+            <Link className="column__link" to="/">Chính sách riêng tư</Link>
+          </p>
         </Col>
         <Col className="column" span={6}>
           <p style={{ fontSize: 20 }}>HỖ TRỢ KHÁCH HÀNG</p>
-          <p>Trung tâm trợ giúp</p>
-          <p>Liên hệ hỗ trợ</p>
-          <p>Hotline: 0963858xxx</p>
+          <p>
+            <Link className="column__link" to="/">Trung tâm trợ giúp</Link>
+          </p>
+          <p>
+            <Link className="column__link" to="/">Liên hệ hỗ trợ</Link>
+          </p>
+          <p>
+            <Link className="column__link" to="/">Hotline: 0963858xxx</Link>
+          </p>
         </Col>
         <Col className="column" span={6}>
           <p style={{ fontSize: 20 }}>VỀ CHÚNG TÔI</p>
-          <p>Giới thiệu</p>
-          <p>Tuyển dụng</p>
-          <p>Tryền thông</p>
+          <p>
+            <Link className="column__link" to="/">Giới thiệu</Link>
+          </p>
+          <p>
+            <Link className="column__link" to="/">Tuyển dụng</Link>
+          </p>
+          <p>
+            <Link className="column__link" to="/">Truyền thông</Link>
+          </p>
         </Col>
         <Col className="column" span={6}>
           <p style={{ fontSize: 20 }}>LIÊN KẾT</p>
           <p>
             <Space size="middle">
-              <FacebookOutlined style={{fontSize: 30}} />
-              <YoutubeOutlined style={{fontSize: 30}} />
-              <GooglePlusOutlined style={{fontSize: 30}} />
+              <Link to="/" className="column__link"><FacebookOutlined style={{ fontSize: 30 }} /></Link>
+              <Link to="/" className="column__link"><YoutubeOutlined style={{ fontSize: 30 }} /></Link>
+              <Link to="/" className="column__link"><GooglePlusOutlined style={{ fontSize: 30 }} /></Link>
             </Space>
           </p>
         </Col>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,7 +5,7 @@ import {
   HomeOutlined,
   UserOutlined,
   BellOutlined,
-  MoreOutlined,
+  BarsOutlined,
   FilterFilled,
   SearchOutlined,
   FormOutlined,
@@ -20,7 +20,7 @@ function Header() {
         <Col span={12} className="content">
           <Row align="bottom">
             <Col span={7} className="content__img">
-              <img src={images.LOGO}/>
+              <img src={images.LOGO} />
             </Col>
             <Col span={17} className="content__label">
               <Space direction="vertical">
@@ -35,22 +35,22 @@ function Header() {
             <ul>
               <li>
                 <Link className="link" to="/">
-                  <HomeOutlined /> Trang chủ
+                  <HomeOutlined style={{ fontSize: 20 }} /> Trang chủ
                 </Link>
               </li>
               <li>
                 <Link className="link" to="/quan-ly">
-                  <UserOutlined /> Quản lý
+                  <UserOutlined style={{ fontSize: 20 }} /> Quản lý
                 </Link>
               </li>
               <li>
                 <Link className="link" to="/thong-bao">
-                  <BellOutlined /> Thông báo
+                  <BellOutlined style={{ fontSize: 20 }} /> Thông báo
                 </Link>
               </li>
               <li>
                 <Link className="link" to="/them">
-                  <MoreOutlined /> Thêm
+                  <BarsOutlined style={{ fontSize: 20 }} /> Thêm
                 </Link>
               </li>
             </ul>
@@ -78,16 +78,13 @@ function Header() {
           </Col>
           <Col span={9} className="sub-func">
             <Space size="large">
-              <div className="sub-func__login">
-                <UserOutlined style={{ color: "#2b78e4" }} />
-                &ensp;
-                <Link to="user-login" style={{ color: "#2b78e4" }}>
-                  Đăng nhập
-                </Link>
-              </div>
+              <Link className="sub-func__login" to="user-login">
+                <UserOutlined style={{ color: "#ffffff" }} />
+                &nbsp; Đăng nhập
+              </Link>
               <Button type="primary" danger>
                 <FormOutlined />
-                Đăng tin
+                &nbsp; Đăng tin
               </Button>
             </Space>
           </Col>
