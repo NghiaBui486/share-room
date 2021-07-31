7ab035a5fa2e1f235c64713be5d3104e74ac9a3e
KhanhBui <buidunamkhanh4126@gmail.com>
2021-07-31 10:51:22 +0700

    handle the function of login, register and log out, update the color of pages

--- src/features/Login/index.jsx
+++ src/features/Login/index.jsx
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
 import { useHistory } from "react-router-dom";
 import { Form, Input, Button, Row, Col, Space } from "antd";
 import "./index.scss";
 import images from "../../contants/images";
+import userApi from "../../api/userApi";
 
 function UserLogin() {
   let history = useHistory();
+  const [userName, setUserName] = useState(null);
+  const [password, setPassword] = useState(null);
+  useMemo(() => {
+    if (localStorage.getItem("token")) {
+      history.push("/");
+    }
+  });
   const redirectRegiter = () => {
     history.push("/user-register");
   };
   const redirectHomePage = () => {
     history.push("/");
-  }
+  };
+  //---handler login
+  const onFinish = () => {
+    const passwordDB = window.btoa(userName + ":" + password);
+    const inFo = {
+      username: userName,
+      password: passwordDB,
+    };
+    userApi
+      .authenticate(inFo)
+      .then((res) => {
+        if (res.token) {
+          localStorage.setItem("token", res.token);
+          localStorage.setItem("name", res.name);
+          history.push("/");
+        }
+      })
+      .catch(() => {
+        console.log("Tai Khoan Khong Ton Tai");
+      });
+  };
+
   return (
     <>
       <div className="wrapper-login">
@@ -22,16 +51,54 @@ function UserLogin() {
                 <h1>Sign In Free</h1>
               </div>
               <div>
-                <Form layout="vertical">
-                  <Form.Item label="User name">
-                    <Input placeholder="Please enter username" />
+                <Form onFinish={onFinish} layout="vertical">
+                  <Form.Item
+                    label="Username"
+                    name="userName"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Username không được để trống",
+                      },
+                      {
+                        pattern: new RegExp(/^[a-zA-Z0-9]+$/i),
+                        message: "Tên đăng nhập không chứa kí tự đặc biệt",
+                      },
+                    ]}
+                  >
+                    <Input
+                      onChange={(e) => {
+                        setUserName(e.target.value);
+                      }}
+                      placeholder="Please enter username"
+                    />
                   </Form.Item>
-                  <Form.Item label="Password">
-                    <Input.Password placeholder="Password" />
+                  <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Password không được để trống",
+                      },
+                      {
+                        pattern: new RegExp(/^[a-zA-Z0-9]+$/i),
+                        message: "Mật khẩu không chứa kí tự đặc biệt",
+                      },
+                    ]}
+                  >
+                    <Input.Password
+                      onChange={(e) => {
+                        setPassword(e.target.value);
+                      }}
+                      placeholder="Password"
+                    />
                   </Form.Item>
                   <Form.Item>
                     <Space>
-                      <Button type="primary">Sign In</Button>
+                      <Button htmlType="submit" type="primary">
+                        Sign In
+                      </Button>
                       <Button onClick={redirectRegiter}>Register</Button>
                     </Space>
                   </Form.Item>
@@ -48,12 +115,12 @@ function UserLogin() {
                 <source
                   width="500px"
                   media="(min-width: 1125px)"
-                  srcset={images.LOGO}
+                  srcSet={images.LOGO}
                 />
                 <source
                   width="290px"
                   media="(min-width: 641px)"
-                  srcset={images.LOGO}
+                  srcSet={images.LOGO}
                 />
                 <img width="500px" src={images.LOGO} />
               </picture>
