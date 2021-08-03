b0dc052ba55241b897092fe558373598d90cdd59
Nghia Bui Van <bvnghia@tma.com.vn>
2021-08-03 08:25:57 +0700

    Revert "design interface body shareroom"
    This reverts commit 564b3d0fcee2359bcc67f464da1790562aff986c.

--- src/features/ShareRoom/page/main/index.jsx
+++ src/features/ShareRoom/page/main/index.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 import Header from "../../../../components/Header"
 import Footer from "../../../../components/Footer"
-import Filter from "../../../../features/ShareRoom/Component_Body/Filter"
-import List_post from "../../Component_Body/List"
-import New from "../../Component_Body/News"
-import { Row, Col, Space } from "antd";
-import Category from "../../Component_Body/Category";
+
 
 function MainPage() {
   let history = useHistory();
@@ -16,30 +12,9 @@ function MainPage() {
 
   return (
     <div className="wrapper-home">
-      
-            <Header/>
-      <div style={{height:20}}></div>
-      
-      <div className='body'>
-        <Row>
-        <Filter/>
-        </Row>
-
-        <Row>
-        <Col span='13' offset={2}> 
-            <List_post/>
-        </Col>
-
-        <Col span='7' offset={1} > 
-           <New/>
-           <Category/>
-        </Col>
-        </Row>
-      </div>
-        
+      <Header />
       <div style={{height: 200}}></div>
-            <Footer />
-  
+      <Footer />
     </div>
   );
 }
